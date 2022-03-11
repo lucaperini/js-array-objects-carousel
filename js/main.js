9ac88efc35cdfb2d997f149614add3b9f53ad84a
lucaperini <luca.perini@gmail.com>
2022-03-11 07:36:53 +0100

    aggiunta funzionalità slider

--- js/main.js
+++ js/main.js
@@ -1,24 +1,4 @@
-/**
- *
- *
- *
-Riprendiamo l'esercizio carosello e rifacciamolo, questa volta usando gli oggetti, prendendo come riferimento il codice scritto oggi insieme a lezione, che troverete direttamente nella mia repository di github a questo link: [link github]
-
-Modifichiamo il codice dell'esercizio per renderlo funzionante con un array di oggetti al posto dei tre array separati, con una sola regola: non è possibile modificare l'HTML ma solamente JS e CSS.
-Ricordiamo sempre l'importanza dell'integrità del dato.
-
-Bonus 1:
-Sperimentiamo attraverso l'uso delle timing functions anche una funzionalità di scorrimento al nostro carosello:
-al click di un bottone o già dall'inizio possiamo far partire, ad intervalli di tempo a piacere, lo scorrimento delle immagini disponibili nel carosello stesso.
-
-Bonus 2:
-E se volessi un bottone per invertire la "direzione" del carosello?
-
- *
- */
-
 // Creo Array di oggetti slider
-
 const slider = [
     {
         image: 'https://picsum.photos/750/540?random=1',
@@ -56,8 +36,6 @@ const slider = [
     
 ]
 
-
-
 //inserisco immagini tramite ciclo
 for (let i = 0; i < slider.length; i++){
     document.querySelector('div#my-carousel .my-carousel-images').innerHTML += 
@@ -67,12 +45,58 @@ for (let i = 0; i < slider.length; i++){
     <p>${slider[i].description}<p>
     </div`
 }
+
 for (let i = 0; i < slider.length; i++){
     document.querySelector('.my-thumbnails').innerHTML += 
-    `<div class="ciccio">
+    `<div class="thumbnail-box">
     <img src="${slider[i].image}" class="thumbnail-item" alt="random picture">
     </div>`
 }
+
+let activeElement = 0;
+
+document.getElementsByClassName('picture')[activeElement].classList.add('active');
+
+document.getElementsByClassName('thumbnail-item')[activeElement].classList.add('active');
+
+const prev = document.querySelector('div.my-previous');
+
+prev.addEventListener('click', function() {
+    
+    document.getElementsByClassName('picture')[activeElement].classList.remove('active');
+    document.getElementsByClassName('thumbnail-item')[activeElement].classList.remove('active');
+
+    
+    if( activeElement === 0){
+        activeElement = slider.length - 1;
+    } else {
+        activeElement--;
+    }
+    
+    document.getElementsByClassName('picture')[activeElement].classList.add('active');
+    document.getElementsByClassName('thumbnail-item')[activeElement].classList.add('active');
+});
+
+const next = document.querySelector('div.my-next');
+
+next.addEventListener('click', function() {
+    
+    document.getElementsByClassName('picture')[activeElement].classList.remove('active');
+    document.getElementsByClassName('thumbnail-item')[activeElement].classList.remove('active');
+
+    if( activeElement === slider.length - 1){
+        activeElement = 0;
+    } else {
+        activeElement++;
+    }
+    
+    
+    document.getElementsByClassName('picture')[activeElement].classList.add('active');
+    document.getElementsByClassName('thumbnail-item')[activeElement].classList.add('active');
+});
+
+
+
 
 
 
